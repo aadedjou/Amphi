b2729d1f2fa14d6fe239ed61858d056d5445a353
adam <66301048+aadedjou@users.noreply.github.com>
2021-05-14 04:08:20 +0200

    Update rendering

--- amphi-app/src/app/interface/interface.component.ts
+++ amphi-app/src/app/interface/interface.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {  interval } from 'rxjs';
 
 @Component({
   selector: 'app-interface',
   templateUrl: './interface.component.html',
   styleUrls: ['./interface.component.scss']
 })
-export class InterfaceComponent implements OnInit {
 
-  constructor() { }
+export class InterfaceComponent implements OnInit {
+  current_seconds = new Date().toLocaleTimeString().split(":").pop();
+  current_time : String = new Date().toLocaleTimeString().slice(0, -3);
+  prof_name : String = "Champalle";
+
+  update_time = interval(1000).subscribe(
+    () => { this.current_time = new Date().toLocaleTimeString().slice(0, -3); }
+  );
+
+  update_seconds = interval(1000).subscribe(
+    () => { this.current_seconds = new Date().toLocaleTimeString().split(":").pop(); }
+  );
+
+    constructor() { }
 
   ngOnInit(): void {
   }
-
 }
+
